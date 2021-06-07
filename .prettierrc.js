ebb89d98155dd8d857054e93880a32e9a5d84069
alexxuss1996 <alekseymelnych132@gmail.com>
2021-06-07 14:48:03 +0300

    Add custom prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	semi: false,
+
+	tabWidth: 4,
+
+	useTabs: false,
+
+	trailingComma: 'es5',
+
+	printWidth: 80,
+
+	endOfLine: 'auto',
+
+	singleQuote: true,
+
+	bracketSpacing: true,
+
+	arrowParens: 'always'
+
+};
